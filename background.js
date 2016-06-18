ec052680aa096b21f34be8dd6207f2372f8cf017
fly9i <fly9i@163.com>
2016-06-18 16:16:58 +0800

    update readme

---
+++ background.js
@@ -0,0 +1,25 @@
+(function(){
+	$('#setting').click(function(){
+		window.open(chrome.extension.getURL('setting.html'));
+	})
+
+	$('#color').on('input',function(){
+		var val = $(this).val();
+		if(val.match(/[0-9a-z]{6}/gi)){
+			$('#c_dis').css({'background-color':'#'+val,"border-color":'#'+val})
+			 chrome.storage.sync.set({'ban_color': val}, function() {
+	          
+	        });
+		}
+	})
+	chrome.storage.sync.get('ban_color',function(res){
+		if(res.ban_color){
+			console.log(res.ban_color)
+			val = res.ban_color
+			$('#c_dis').css({'background-color':'#'+val,"border-color":'#'+val})
+			$('#color').val(res.ban_color)
+		}
+
+	})
+	
+})();
